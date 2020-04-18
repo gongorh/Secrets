8ff85875ecd5d3ba55a2894aed848a8ac0686fd9
Homero Gongora <gongorh1@gmail.com>
2020-04-18 15:41:46 -0400

    Add API Key

--- app.js
+++ app.js
@@ -15,36 +15,36 @@ app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
 });
 
 userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res) {
+app.get("/login", function (req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res) {
+app.get("/register", function (req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res) {
+app.post("/register", function (req, res) {
   const newUser = User({
     email: req.body.username,
-    password: req.body.password
+    password: req.body.password,
   });
-  newUser.save(function(err) {
+  newUser.save(function (err) {
     if (err) {
       console.log(err);
     } else {
@@ -53,12 +53,12 @@ app.post("/register", function(req, res) {
   });
 });
 
-app.post("/login", function(req, res) {
+app.post("/login", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
   console.log(username);
   console.log(password);
-  User.findOne({ email: username }, function(err, foundUser) {
+  User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
       console.log(err);
     } else {
@@ -73,6 +73,6 @@ app.post("/login", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
